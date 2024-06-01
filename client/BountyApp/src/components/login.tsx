4967446c6e2493d7571d2d8be70bb6bc34e2c1c3
nathank <nathan071303@gmail.com>
2024-06-01 13:03:19 -0500

    edits to login

--- client/BountyApp/src/components/login.tsx
+++ client/BountyApp/src/components/login.tsx
@@ -1,81 +1,94 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Login = (props: {setLoggedIn: any, setUsername: any}) => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [usernameError, setUsernameError] = useState('')
-  const [passwordError, setPasswordError] = useState('')
+const Login = (props: { setLoggedIn: any; setUsername: any }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const onButtonClick = () => {
-
     // Validations
-    setUsernameError('')
-    setPasswordError('')
-  
-    if ('' === username) {
-      setUsernameError('Please enter a username')
-      console.log('Please enter a username')
-      return
+    setUsernameError("");
+    setPasswordError("");
+
+    if ("" === username) {
+      setUsernameError("Please enter a username");
+      console.log("Please enter a username");
+      return;
     }
-  
-    if ('' === password) {
-      setPasswordError('Please enter a password')
-      console.log('Please enter a password')
-      return
+
+    if ("" === password) {
+      setPasswordError("Please enter a password");
+      console.log("Please enter a password");
+      return;
     }
-  
+
     // Authentication
-    fetch('http://localhost:5000/api/user/login', {
-      method: 'POST',
+    fetch("http://localhost:5000/api/user/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         username: username,
-        password: password
-      })
+        password: password,
+      }),
     })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
-
-
-  }
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Invalid username or password");
+        }
+        response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+        window.alert("Invalid username or password");
+      });
+  };
 
   return (
-    <div className={'mainContainer'}>
-      <div className={'titleContainer'}>
+    <div className={"mainContainer"}>
+      <div className={"titleContainer"}>
         <div>Login</div>
       </div>
       <br />
-      <div className={'inputContainer'}>
+      <div className={"inputContainer"}>
         <input
           value={username}
           placeholder="Enter your username here"
           onChange={(ev) => setUsername(ev.target.value)}
-          className={'inputBox'}
+          className={"inputBox"}
         />
         <label className="errorLabel">{usernameError}</label>
       </div>
       <br />
-      <div className={'inputContainer'}>
+      <div className={"inputContainer"}>
         <input
           value={password}
           placeholder="Enter your password here"
           onChange={(ev) => setPassword(ev.target.value)}
-          className={'inputBox'}
+          className={"inputBox"}
         />
         <label className="errorLabel">{passwordError}</label>
       </div>
       <br />
-      <div className={'inputContainer'}>
-        <input className={'inputButton'} type="button" onClick={onButtonClick} value={'Log in'} />
+      <div className={"inputContainer"}>
+        <input
+          className={"inputButton"}
+          type="button"
+          onClick={onButtonClick}
+          value={"Log in"}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
